311c8fe70e9f2b9c37901744f1cf8caa84c9f2d8
lenin <lnnsntg@gmail.com>
2021-08-07 17:32:33 +0200

    at 74 agregando useEffect

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,21 +1,25 @@
 import React, {useState}from "react";
+import {AddCategory } from './components/AddCategory'
+import { GifGrid } from "./components/GifGrid";
+
 
 export const GifExpertApp = () => {
-  //const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
-  const [categories, setCategories] = useState(["One Punch", "Samurai X", "Dragon Ball" ]);
-  const handleAdd = () => {
-    setCategories(state => [...categories, "hunter"  ]);
-  };
+  const [categories, setCategories] = useState(["One Punch" ]);
+
   return (
     <>
       <div>
         <h2>GifExpertApp</h2>
+        <AddCategory setCategories={setCategories} />
         <hr />
-        <button onClick={handleAdd}>Agregar</button>
         <ol>
-          {categories.map((category) => {
-            return <li key={category}>{category}</li>;
-          })}
+          {
+            categories.map((category) => (
+              <GifGrid 
+              key={ category }
+              category={ category } />          
+          ))
+          }
         </ol>
       </div>
     </>
---
+++ src/components/GifGrid.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+export const GifGrid = ({ category }) => {
+
+  const [count, setCount] = useState(0);
+  useEffect( () => {
+    getGifs();
+  },[])
+
+  const getGifs = async () => {
+    const url =
+      "https://api.giphy.com/v1/gifs/search?q=rick+and+morty&limit=10&api_key=F9XVGsO4DCJyBJjgVFJvkgOcVhz9MWVA";
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+    const gifs = data.map((img) => {
+      return {
+        id: img.id,
+        title: img.title,
+        url: img.images?.downsized_medium.url,
+      };
+    });
+    console.log(gifs);
+  };
+
+  //getGifs();
+
+  return (
+    <div>
+      <h3>{ category }</h3>
+      <h3>{ count }</h3>
+      <button onClick={() => setCount(count + 1)}>count</button>
+    </div>
+  );
+};
